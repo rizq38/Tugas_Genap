d05e24e5bdc29fdb18562b226d26e21f9dd3a291
rizq38 <143860236+rizq38@users.noreply.github.com>
2023-09-07 20:51:40 +0700

    Add files via upload

---
+++ Tugas_genap/Hitung_Volume_Balok.py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Balok
+
+print("Program Menghitung Volume Balok")
+
+p = int(input("Masukkan Panjang Balok : "))
+l = int(input("Masukkan Lebar Balok : "))
+t = int(input("Masukkan Tinggi Balok : "))
+
+V = p * l * t
+print("Rumus Menghitung Volume Balok V = p x l x t")
+print("Hasil dari Hitung Volume Balok : ",V)
---
+++ Tugas_genap/Hitung_Volume_Tabung.py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Tabung
+
+print("Program Menghitung Volume Tabung")
+
+pi = 3.14
+r = int(input("Masukkan Jari-jari Tabung : "))
+t = int(input("Masukkan Tinggi Tabung : "))
+
+V = pi * r ** 2 * t
+print("Rumus Menghitung Volume Tabung V = pi x r**2 x t")
+print("Hasil dari Hitung Volume Tabung : ",V)
